cb53780c47c93db4e4242d0b5d32aecc5dabc39e
Rui Moreno <ruiguemo@gmail.com>
2021-09-26 22:09:03 -0300

    🚧 analysing line 36. In attribute curr_id, "reports" was change to "curr_address"

--- check.py
+++ check.py
@@ -6,31 +6,33 @@ from lxml import etree
 from sys import exit
 from time import time
 import os
-reports = glob('mp-report-*.xml')
-print (reports)
+
+reports = glob('mesh-report-*.xml')
 ids = dict()
+
 for report in reports:
-        aux = open(report).read()
-        meshPointDevice = etree.XML(aux)
-        curr_address = meshPointDevice.get('address')
-        curr_id = int(filter(str.isdigit, report))
-        ids[curr_address] = curr_id
+    aux = open(report).read()
+    meshPointDevice = etree.XML(aux)
+    curr_address = meshPointDevice.get('address')
+    curr_id = filter(str.isdigit, curr_address)
+    ids[curr_address] = curr_id
 
 G = nx.Graph()
 for report in reports:
-        aux = open(report).read()
-        meshPointDevice = etree.XML(aux)
-        currAddress = meshPointDevice.get('address')
-        currId = ids[currAddress]
-        G.add_node(currId)
+    aux = open(report).read()
+    meshPointDevice = etree.XML(aux)
+    currAddress = meshPointDevice.get('address')
+    currId = ids[currAddress]
+    G.add_node(currId)
 
-        peerManagementProtocol = meshPointDevice.find('PeerManagementProtocol')
-        links = peerManagementProtocol.findall('PeerLink')
-        for link in links:
-                peerAddress = link.get('peerMeshPointAddress')
-                peerId = ids[peerAddress]
-                G.add_edge(currId, peerId)
+    peerManagementProtocol = meshPointDevice.find('PeerManagementProtocol')
+    links = peerManagementProtocol.findall('PeerLink')
+    for link in links:
+        peerAddress = link.get('peerMeshPointAddress')
+        peerId = ids[peerAddress]
+        G.add_edge(currId, peerId)
+        
 print ('Media num conexoes:', float(len(G.edges()))/float(len(G.nodes())))
 nx.write_dot(G, 'checked_%d.dot' % int(time()))
 if not nx.is_connected(G):
-        raise Exception("[check.py] invalid topology!")
+    raise Exception("[check.py] invalid topology!")
